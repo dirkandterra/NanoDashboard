21173fca19842b9fb0e5fd49ac85d74ea1065b7f
DR <dirkandterra@aol.com>
2025-01-27 19:49:46 -0600

    500 to get 250k

--- ArduinoPastPrograms/NanoCAN/MCP_CAN_lib-master/examples/CAN_send/CAN_send.ino
+++ ArduinoPastPrograms/NanoCAN/MCP_CAN_lib-master/examples/CAN_send/CAN_send.ino
@@ -10,7 +10,7 @@ void setup()
 {
   uint8_t error;
   Serial.begin(115200);
-  error=CAN0.begin(MCP_ANY, CAN_500KBPS, MCP_8MHZ);
+  error=CAN0.begin(MCP_ANY, CAN_250KBPS, MCP_8MHZ);
   // Initialize MCP2515 running at 16MHz with a baudrate of 500kb/s and the masks and filters disabled.
   if(error == CAN_OK) Serial.println("MCP2515 Initialized Successfully!");
   else{
